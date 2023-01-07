4d9f7db09b5d3245067fefe1fdefc6d72387d2bd
Maxwell Lysle <max@lysle.org>
2023-01-07 16:41:29 -0700

    finished 1.34

---
+++ 1.3/ex-1.34.rkt
@@ -0,0 +1,12 @@
+#lang sicp
+; 1.34
+; Q: What happens if we (perversely) ask the interpreter to evaluate the combination (f f)? Explain.
+
+; A: (f f) will substitute the argument f for the formal paramter g in the procedure body (g 2).
+; Then the interpreter will apply f with the argument 2. This substitutes 2 for g in the body (g 2).
+; (2 2) will fail to evaluate because 2 is not a preocedure.
+; (f f) -> (f 2) -> (2 2) -> ERROR
+(define (f g) (g 2))
+(f f)
+
+(f (lambda (x) (* x x)))
