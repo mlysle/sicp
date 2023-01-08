7d9e9aa8c2a09eac506e3e796195d6437de93e0d
Maxwell Lysle <max@lysle.org>
2023-01-07 21:02:25 -0700

    3 more exercises complete

---
+++ 1.3/ex-1.35.rkt
@@ -0,0 +1,17 @@
+#lang sicp
+
+; Show the golden ratio is a fixed point of f(x) = 1 + 1/x
+
+(define tolerance 0.0001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
