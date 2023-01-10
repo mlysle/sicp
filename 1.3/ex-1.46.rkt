6a8f49e0d75d5327f0abbf04c7a13edfb9cf5c91
Maxwell Lysle <max@lysle.org>
2023-01-09 20:27:43 -0700

    finished 1.46 part 2

--- 1.3/ex-1.46.rkt
+++ 1.3/ex-1.46.rkt
@@ -1,24 +1,38 @@
 #lang sicp
 
+; Exercise 1.46
+
 (define (average x y) (/ (+ x y) 2))
 
 (define (iterative-improve good-enough? improve)
-  (lambda (guess x)
-    (if (good-enough? guess x)
+  (lambda (guess)
+    (if (good-enough? guess)
       guess
       ((iterative-improve
          good-enough?
-         improve)
-       (improve guess x) x))))
+         improve) (improve guess)))))
 
-(define (new-sqrt x)
+(define (sqrt-iter x)
   (define initial-guess 1.0)
-  (define (good-enough? guess x)
-    (if (< (abs (- (improve guess x) guess)) 0.001)
-      #t
-      #f))
-  (define (improve guess x)
+  (define (good-enough? guess)
+    (< (abs (- (improve guess) guess)) 0.001))
+  (define (improve guess)
     (average guess (/ x guess)))
-  ((iterative-improve good-enough? improve) initial-guess x))
+  ((iterative-improve good-enough? improve) initial-guess))
 
-(new-sqrt 100)
+(define (fixed-point-iter f)
+  (define initial-guess 1.0)
+  (define tolerance 0.00001)
+  (define (good-enough? guess)
+    ;(display (< (abs (- guess (improve guess))) tolerance))
+    (newline)
+    (< (abs (- guess (improve guess))) tolerance))
+  (define (improve guess)
+    (f guess))
+  ((iterative-improve good-enough? f) initial-guess))
+
+(sqrt-iter 100)
+
+; while this works, I can't seem to redefine sqrt in terms of fixed-point-iter like in 1.33.
+; though luckily the exercise doesn't specify anything about that.
+(fixed-point-iter cos)
