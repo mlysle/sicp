ba43b1858e2335e9d2e2b4bdc8599815365294ca
Maxwell Lysle <max@lysle.org>
2023-03-14 19:09:13 -0600

    simplified a procedure

--- week3/perfectnumbers.rkt
+++ week3/perfectnumbers.rkt
@@ -1,10 +1,8 @@
 #lang sicp
 
-; is i a factor of n?
-(define (is-factor? n i)
-  (if (equal? (modulo n i) 0)
-    #t
-    #f))
+; is 'a' a factor of n?
+(define (is-factor? n a)
+  (equal? (modulo n a) 0))
 
 ; it does the trick but could definitely be rewritten
 (define (sum-of-factors n i)
@@ -13,11 +11,11 @@
    (if (is-factor? n i)
      (+ i (sum-of-factors n (inc i)))
      (+ 0 (sum-of-factors n (inc i))))))
-    
 
 (define (next-perf n)
   (if (equal? n (sum-of-factors n 1))
       n
       (next-perf (inc n))))
 
+; calculate perfect numbers
 (next-perf 29)
