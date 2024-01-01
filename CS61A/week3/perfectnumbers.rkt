b4421ee95692bfcdc58e67967f3dbec538731591
Maxwell Lysle <max@lysle.org>
2024-01-01 14:29:29 -0700

    rewrite using iteration

--- CS61A/week3/perfectnumbers.rkt
+++ CS61A/week3/perfectnumbers.rkt
@@ -1,19 +1,18 @@
 #lang sicp
 
 ; is 'a' a factor of n?
-(define (is-factor? n a)
+(define (is-factor? a n)
   (equal? (modulo n a) 0))
 
-; it does the trick but could definitely be rewritten
-(define (sum-of-factors n i)
-  (if (equal? n i)
-    0
-   (if (is-factor? n i)
-     (+ i (sum-of-factors n (inc i)))
-     (+ 0 (sum-of-factors n (inc i))))))
+(define (sum-of-factors n i sum)
+  (if (equal? i n)
+    sum
+    (if (is-factor? i n)
+      (sum-of-factors n (inc i) (+ sum i))
+      (sum-of-factors n (inc i) sum))))
 
 (define (next-perf n)
-  (if (equal? n (sum-of-factors n 1))
+  (if (equal? n (sum-of-factors n 1 0))
       n
       (next-perf (inc n))))
 
